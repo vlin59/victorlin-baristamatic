65cc5332af74692ce7f9c9c5b3877c0976feb347
vlin59 <vlin59@gmail.com>
2016-12-04 17:26:11 -0600

    Fix server variables for deployment

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(3000, function(err) {
+app.listen(process.env.PORT || 3000, function(err) {
   if (err) {
     return console.error(err);
   }
